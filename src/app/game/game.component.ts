2c11b8fa4891f3449e567f570040f547c2773ebc
Neesou <93050748+Neesou@users.noreply.github.com>
2023-09-20 10:51:09 +0200

    end Project Singel Play Desctop

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, inject  } from '@angular/core';
+import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit{
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor(public dialog: MatDialog){ 
+    this.game = new Game();
+   }
+
+  ngOnInit(): void {
+    this.newGame();   
+    
+  }
+
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game)
+  }
+  takeCard() {
+    if(!this.pickCardAnimation){
+      this.currentCard = this.game.stack.pop()?.toString() ?? '';
+      console.log(this.currentCard)
+      this.pickCardAnimation = true;
+      this.game.currentPlayer++
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
+  
+      setTimeout(()=>{
+        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.currentCard);
+      },2500)
+    }
+    
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if(name && name.length > 0){
+        this.game.players.push(name)
+      } 
+    });
+  }
+}
